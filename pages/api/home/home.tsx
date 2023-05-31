c260caebc8435ad854c08d8e4986d0f19535a9a4
bitman333 <76186250+bitman333@users.noreply.github.com>
2023-05-30 22:34:37 -0600

    Update home.tsx
    First Commit

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -360,8 +360,8 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>Elerion Chat</title>
+        <meta name="description" content="Elerion Chat" />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
